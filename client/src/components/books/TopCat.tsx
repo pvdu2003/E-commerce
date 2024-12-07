c60548f027f7f1e2ffc882fd6320dc4c071381df
pvdu2003 <152840528+pvdu2003@users.noreply.github.com>
2024-12-07 10:28:17 +0700

    FE: Update responsive UI for home page

--- client/src/components/books/TopCat.tsx
+++ client/src/components/books/TopCat.tsx
@@ -24,14 +24,19 @@ interface CategorySectionProps {
 const TopCat: React.FC<CategorySectionProps> = ({ cat }) => {
   return (
     <Box key={cat.cat_id} className="my-3 py-2 px-3 bg-light">
-      <Box display="flex" justifyContent="space-between" pb={2}>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        pb={2}
+        alignItems="center"
+      >
         <Typography variant="h5" component="h2">
-          <a
-            href={`/category/${cat.cat_id}`}
+          <Link
+            to={`/category/${cat.cat_id}`}
             className="text-main text-decoration-none"
           >
             {cat.category}
-          </a>
+          </Link>
         </Typography>
         <Link
           to={`/category/${cat.cat_id}`}
@@ -46,14 +51,15 @@ const TopCat: React.FC<CategorySectionProps> = ({ cat }) => {
       </Box>
       <Grid container spacing={2}>
         {cat.books.map((book) => (
-          <BookCard
-            key={book._id}
-            _id={book._id}
-            image={book.image}
-            title={book.title}
-            price={book.price}
-            quantity_sold={book.quantity_sold}
-          />
+          <Grid item xs={12} sm={6} md={4} lg={2} key={book._id}>
+            <BookCard
+              _id={book._id}
+              image={book.image}
+              title={book.title}
+              price={book.price}
+              quantity_sold={book.quantity_sold}
+            />
+          </Grid>
         ))}
       </Grid>
     </Box>
